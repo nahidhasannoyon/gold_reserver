c042e39e565698a99f6dad11549dd5abf1d7b5fe
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-08-15 23:43:08 +0600

    adding more screen's route

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:gold_reserver/page_not_ready.dart';
 import 'package:gold_reserver/screens/add_cash_screen/add_cash_screen.dart';
+import 'package:gold_reserver/screens/buy_sell_gold_screen/buy_gold_screen.dart';
 import 'package:gold_reserver/screens/buy_sell_gold_screen/buy_sell_gold_screen.dart';
+import 'package:gold_reserver/screens/buy_sell_gold_screen/sell_gold_screen.dart';
 import 'package:gold_reserver/screens/dashboard_screen/add_new_mission.dart';
 import 'package:gold_reserver/screens/dashboard_screen/dashboard_screen.dart';
+import 'package:gold_reserver/screens/edit_profile_screen.dart/edit_profile_screen.dart';
 import 'package:gold_reserver/screens/history_screen/history_screen.dart';
 import 'package:gold_reserver/screens/profile_screen.dart';
+import 'package:gold_reserver/screens/selected_shop_screen/selected_shop_screen.dart';
+import 'package:gold_reserver/screens/withdraw_screen.dart/withdraw_screen.dart';
+import 'package:gold_reserver/screens/withdraw_selection_screen/withdraw_selection_screen.dart';
 import 'package:gold_reserver/screens/your_wallet_screen/your_wallet_screen.dart';
 
 void main() {
@@ -34,6 +40,13 @@ class MyApp extends StatelessWidget {
         AddNewMission.routeName: (context) => const AddNewMission(),
         PageNotReady.routeName: (context) => const PageNotReady(),
         BuySellGoldScreen.routeName: (context) => const BuySellGoldScreen(),
+        BuyGoldScreen.routeName: (context) => const BuyGoldScreen(),
+        SellGoldScreen.routeName: (context) => const SellGoldScreen(),
+        EditProfileScreen.routeName: (context) => const EditProfileScreen(),
+        WithDrawScreen.routeName: (context) => const WithDrawScreen(),
+        WithDrawSelectionScreen.routeName: (context) =>
+            const WithDrawSelectionScreen(),
+        SelectedShopScreen.routeName: (context) => const SelectedShopScreen(),
       },
     );
   }
