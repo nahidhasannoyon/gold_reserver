06426ce6693604e70d9151e4bd87929c283570d8
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-08-15 11:27:27 +0600

    adding a dummy page to avoid adding null to button

---
+++ lib/page_not_ready.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class PageNotReady extends StatelessWidget {
+  static const routeName = '/page_not_ready';
+  const PageNotReady({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Page not ready'),
+      ),
+      body: const Center(
+        child: Text(
+          'Page not ready',
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.red,
+          ),
+        ),
+      ),
+    );
+  }
+}
