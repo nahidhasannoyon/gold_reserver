99e4e6dc0ed718129e2d604f7e2a9f352b3c2db4
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-08-20 21:18:39 +0600

    splitting into widget.

--- lib/screens/your_wallet_screen/your_wallet_screen.dart
+++ lib/screens/your_wallet_screen/your_wallet_screen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:gold_reserver/screens/add_cash_screen/add_cash_screen.dart';
-import 'package:gold_reserver/screens/history_screen/history_screen.dart';
 import 'package:gold_reserver/screens/menu_drawer.dart';
-import 'package:gold_reserver/screens/your_wallet_screen/gold_widget.dart';
-import 'package:gold_reserver/screens/your_wallet_screen/trade_cash_widget.dart';
+import 'package:gold_reserver/screens/your_wallet_screen/your_wallet_widget.dart';
 
 class YourWalletScreen extends StatefulWidget {
   static const routeName = '/your_wallet';
@@ -22,40 +19,10 @@ class _YourWalletScreenState extends State<YourWalletScreen> {
         title: const Text('Your Wallet'),
       ),
       backgroundColor: Colors.grey,
-      body: Container(
-        padding: const EdgeInsets.all(5.0),
+      body: const Padding(
+        padding: EdgeInsets.all(8.0),
         child: SingleChildScrollView(
-          child: Center(
-              child: Card(
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  //* Gold
-                  const GoldWidget(),
-                  //* Trade Cash
-                  const TradeCashWidget(),
-                  // todo CashWidget(),
-                  //*  Add Cash
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).pushNamed(AddCashScreen.routeName);
-                    },
-                    child: const Text('Add Cash'),
-                  ),
-                  //* History
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).pushNamed(HistoryScreen.routeName);
-                    },
-                    child: const Text('History'),
-                  ),
-                ],
-              ),
-            ),
-          )),
+          child: YourWalletWidget(),
         ),
       ),
     );
