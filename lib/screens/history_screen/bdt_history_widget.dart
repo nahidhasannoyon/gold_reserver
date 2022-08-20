2e53998ccb79c863990a02fad51610c1f700b33c
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-08-20 21:12:48 +0600

    making history for page for bdt usage.

---
+++ lib/screens/history_screen/bdt_history_widget.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:gold_reserver/page_not_ready.dart';
+
+class BdtHistoryPage extends StatelessWidget {
+  const BdtHistoryPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Center(
+        child: Column(
+          children: [
+            Card(
+              child: Row(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Text('Filter By'),
+                  ),
+                  const Spacer(),
+                  TextButton(
+                    style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side:
+                                        const BorderSide(color: Colors.red)))),
+                    onPressed: () {
+                      Navigator.of(context).pushNamed(PageNotReady.routeName);
+                    },
+                    child: const Text("All"),
+                  ),
+                  const Spacer(),
+                  TextButton(
+                    style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side:
+                                        const BorderSide(color: Colors.red)))),
+                    onPressed: () {
+                      Navigator.of(context).pushNamed(PageNotReady.routeName);
+                    },
+                    child: const Text("Only Purchased"),
+                  ),
+                  const Spacer(),
+                  TextButton(
+                    style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side:
+                                        const BorderSide(color: Colors.red)))),
+                    onPressed: () {
+                      Navigator.of(context).pushNamed(PageNotReady.routeName);
+                    },
+                    child: const Text("Only Sell"),
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: const [
+                    Text(
+                      'Date time ',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Spacer(),
+                    Text(
+                      'Quantity ',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * .7,
+              child: Expanded(
+                child: ListView.builder(
+                  itemCount: 20,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: const [
+                            Text(
+                              '220-08-2022',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            Spacer(),
+                            Text(
+                              '11090',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+
+                  // child: ListView(
+                  //   children: const [
+                  //     ListTile(
+                  //       leading: Text('2020-01-01'),
+                  //       trailing: Text('100'),
+                  //     ),
+                  //     ListTile(
+                  //       leading: Text('2020-01-01'),
+                  //       trailing: Text('100'),
+                  //     ),
+                  //   ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
