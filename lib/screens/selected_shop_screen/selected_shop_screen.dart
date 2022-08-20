f264d3e3159023e5e5f562d6eafee7265c2c6849
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-08-20 21:14:08 +0600

    page to see selected shop info

---
+++ lib/screens/selected_shop_screen/selected_shop_screen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:gold_reserver/page_not_ready.dart';
+
+class SelectedShopScreen extends StatefulWidget {
+  static const routeName = '/selected_shop';
+  const SelectedShopScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SelectedShopScreen> createState() => _SelectedShopScreenState();
+}
+
+class _SelectedShopScreenState extends State<SelectedShopScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Selected Shop'),
+      ),
+      body: Card(
+        child: Column(
+          children: [
+            Card(
+              child: Column(
+                children: [
+                  const ListTile(
+                    leading: Icon(Icons.shop),
+                    title: Text('Khan Jewler\'s Shop'),
+                    subtitle: Text('Dhaka'),
+                  ),
+                  const Divider(
+                    thickness: 5,
+                  ),
+                  Card(
+                    child: Column(
+                      children: [
+                        Row(
+                          children: const [
+                            Text('Appointment Time:  '),
+                            Expanded(
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  hintText: 'Set appointment time',
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.of(context).pushNamed(
+                                PageNotReady.routeName,
+                              );
+                            },
+                            child: const Text('Cancel Request'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Card(
+                    child: ListTile(
+                      title: Text('Purches request',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          )),
+                      subtitle: Text(
+                        'OTP: 1109020',
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
