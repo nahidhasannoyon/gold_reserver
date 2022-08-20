47b99435616d6725b9cdbcae65e3d34004a41f6c
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-08-20 21:10:31 +0600

    adding wallet to dashboard.

--- lib/screens/dashboard_screen/dashboard_screen.dart
+++ lib/screens/dashboard_screen/dashboard_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:gold_reserver/screens/dashboard_screen/your_mission_widget.dart';
+import 'package:gold_reserver/screens/buy_sell_gold_screen/buy_sell_gold_widget.dart';
+import 'package:gold_reserver/screens/your_mission_Screen/your_mission_widget.dart';
 import 'package:gold_reserver/screens/menu_drawer.dart';
+import 'package:gold_reserver/screens/your_wallet_screen/your_wallet_widget.dart';
 
 class DashboardScreen extends StatefulWidget {
   static const routeName = '/dashboard';
@@ -18,12 +20,17 @@ class _DashboardScreenState extends State<DashboardScreen> {
       appBar: AppBar(
         title: const Text('Dashboard'),
       ),
+      backgroundColor: Colors.grey,
       body: SingleChildScrollView(
-        child: Column(
-          children: const [
-            YourMissionWidget(),
-            SizedBox(height: 10),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: const [
+              YourMissionWidget(),
+              BuySellGoldWidget(),
+              YourWalletWidget(),
+            ],
+          ),
         ),
       ),
     );
