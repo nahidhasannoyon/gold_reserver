ba6236923baea89173a4194204757e998ca76c1a
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-08-20 21:18:58 +0600

    removing dropdown

--- lib/screens/your_wallet_screen/trade_cash_widget.dart
+++ lib/screens/your_wallet_screen/trade_cash_widget.dart
@@ -1,10 +1,16 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class TradeCashWidget extends StatelessWidget {
-  const TradeCashWidget({
-    Key? key,
-  }) : super(key: key);
+class CashCardWidget extends StatelessWidget {
+  final cashType;
+  final cashAmount;
+  final cashDescription;
+
+  const CashCardWidget({
+    super.key,
+    required this.cashType,
+    required this.cashAmount,
+    required this.cashDescription,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -15,50 +21,31 @@ class TradeCashWidget extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            const Expanded(
+            Expanded(
               flex: 2,
               child: Text(
-                "Trade cash: ",
-                style: TextStyle(fontSize: 20),
+                cashType,
+                style: const TextStyle(fontSize: 20),
               ),
             ),
-            const Expanded(
+            Expanded(
               flex: 1,
               child: Text(
-                "0",
-                style: TextStyle(
+                cashAmount,
+                style: const TextStyle(
                   fontSize: 20,
                   color: Colors.yellow,
                 ),
               ),
             ),
-            DropdownButton(
-              hint: const Text('BDT'),
-              // todo jai option select korbe i hsabe hint thbe
-              items: const [
-                DropdownMenuItem(
-                  value: "BDT",
-                  child: Text("BDT"),
-                ),
-                DropdownMenuItem(
-                  value: "USD",
-                  child: Text("USD"),
-                ),
-              ],
-              onChanged: (value) {
-                if (kDebugMode) {
-                  print(value);
-                }
-              },
-            ),
+            const Text('BDT', style: TextStyle(fontSize: 18)),
             IconButton(
               onPressed: () {
                 showDialog(
                   context: context,
                   builder: (context) => AlertDialog(
-                    title: const Text('TRADE CASH'),
-                    content:
-                        const Text('It shows the amount of gold you traded.'),
+                    title: Text(cashType),
+                    content: Text(cashDescription),
                     actions: [
                       ElevatedButton(
                         child: const Text('OK'),
