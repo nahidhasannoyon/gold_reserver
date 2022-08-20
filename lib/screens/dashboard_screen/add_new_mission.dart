93fe4fbfbd9f44361d0cfecf68de5d36aa6784ea
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-08-20 21:09:57 +0600

    adding submit button action, drop down for time range selection,

--- lib/screens/dashboard_screen/add_new_mission.dart
+++ lib/screens/dashboard_screen/add_new_mission.dart
@@ -11,6 +11,7 @@ class AddNewMission extends StatefulWidget {
 }
 
 class _AddNewMissionState extends State<AddNewMission> {
+  final _nameController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime _selectedDate = DateTime.now();
   bool isDatePicked = false;
@@ -36,8 +37,16 @@ class _AddNewMissionState extends State<AddNewMission> {
     );
   }
 
+  void _submitData() {
+    // if (_amountController.text.isNotEmpty && isDatePicked != false)
+    {
+      Navigator.of(context).pop();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    // todo add mission name.
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add New Mission'),
@@ -57,11 +66,30 @@ class _AddNewMissionState extends State<AddNewMission> {
                       children: [
                         Flexible(
                           child: TextFormField(
+                            controller: _nameController,
+                            keyboardType: TextInputType.text,
+                            decoration: const InputDecoration(
+                              border: UnderlineInputBorder(),
+                              labelText: 'Enter Mission Name',
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      children: [
+                        Flexible(
+                          child: TextFormField(
                             controller: _amountController,
                             keyboardType: TextInputType.number,
                             decoration: const InputDecoration(
                               border: UnderlineInputBorder(),
-                              labelText: 'Enter target amount',
+                              labelText: 'Enter Quantity',
                             ),
                           ),
                         ),
@@ -93,6 +121,45 @@ class _AddNewMissionState extends State<AddNewMission> {
                     padding: const EdgeInsets.all(10.0),
                     child: Row(
                       children: [
+                        const Text('Time Range:  '),
+                        Flexible(
+                          child: TextFormField(
+                            controller: _amountController,
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                              border: UnderlineInputBorder(),
+                              labelText: 'Ex. 1,2,3,...',
+                            ),
+                          ),
+                        ),
+                        DropdownButton(
+                          hint: const Text('Month'),
+                          items: const [
+                            DropdownMenuItem(
+                              value: 'Month',
+                              child: Text('Month'),
+                            ),
+                            DropdownMenuItem(
+                              value: 'Year',
+                              child: Text('Year'),
+                            )
+                          ],
+                          onTap: null,
+                          onChanged: ((value) {
+                            if (kDebugMode) {
+                              print(value);
+                            }
+                          }),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      children: [
                         Flexible(
                           fit: FlexFit.tight,
                           child: Text(
@@ -101,6 +168,7 @@ class _AddNewMissionState extends State<AddNewMission> {
                                 : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}',
                           ),
                         ),
+                        // todo date now month and year.
                         TextButton(
                           child: const Text(
                             'Choose Date',
@@ -116,6 +184,28 @@ class _AddNewMissionState extends State<AddNewMission> {
                     ),
                   ),
                 ),
+
+                CheckboxListTile(
+                    title: const Text(
+                        'Get weakly notification to add gold in this mission.'),
+                    value: true,
+                    controlAffinity: ListTileControlAffinity.leading,
+                    onChanged: (value) {
+                      if (kDebugMode) {
+                        print(value);
+                      }
+                    }),
+                Center(
+                  child: ElevatedButton(
+                    // onPressed: _submitData,
+                    onPressed: () {
+                      _submitData();
+                    },
+                    child: const Text("Submit"),
+                  ),
+                ),
+
+                // todo add a check button to notice in month or weakly
               ],
             ),
           ),
